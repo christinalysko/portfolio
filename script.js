b4c0f77af3d9cbc47aa9efeb5e1bdccb5f378840
christinalysko <mothmanowo@gmail.com>
2018-12-03 13:08:47 -0500

    [commit message]

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Get the modal
 var modal = document.getElementById('myModal');
-
+ 
 // Get the image and insert it inside the modal - use its "alt" text as a caption
 var img = document.getElementById('chew');
 var modalImg = document.getElementById("img01");
@@ -10,7 +10,7 @@ img.onclick = function(){
     modalImg.src = this.src;
     captionText.innerHTML = this.alt;
 }
-
+ 
 // fine
 var img = document.getElementById('fine');
 var modalImg = document.getElementById("img01");
@@ -41,6 +41,65 @@ img.onclick = function(){
     captionText.innerHTML = this.alt;
 }
 
+// head
+var img = document.getElementById('head');
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+img.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.alt;
+}
+
+// grasp
+var img = document.getElementById('grasp');
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+img.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.alt;
+}
+
+// moose
+var img = document.getElementById('moose');
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+img.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.alt;
+}
+
+// betta
+var img = document.getElementById('betta');
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+img.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.alt;
+}
+
+// plane
+var img = document.getElementById('plane');
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+img.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.alt;
+}
+
+// me
+var img = document.getElementById('me');
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+img.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.alt;
+}
 
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
@@ -49,5 +108,11 @@ var span = document.getElementsByClassName("close")[0];
 span.onclick = function() { 
     modal.style.display = "none";
 }
+
+var boxid = "myModal";
+ window.onkeyup = function (event) {
+  if (event.keyCode == 27) {
+    document.getElementById(boxid).style.display="none";
+  }}
 
 console.log('is this working?')
